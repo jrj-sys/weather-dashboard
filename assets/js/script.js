e47d39a50eeac0e1b501a5e4522caa48c2f80145
jrj-sys <jamesjamel99@gmail.com>
2022-03-17 02:30:46 -0500

    add dynamic weather display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,35 @@
 // WHEN I search for a city
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
-var getCityWeather = function(city) {
-    // call Weather API for (city) specific weather
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=6f634b702b3d948c5f118672eb3c519f&units=imperial';
+var getCityWeather = function(lat, lon) {
+    // use getCityLatLon latitude and longitude values to input into OneCall API 
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat +'&lon=' + lon +'&appid=6f634b702b3d948c5f118672eb3c519f&units=imperial';
     fetch(apiUrl).then(function(response){
         // request was successful 
         if (response.ok) {
-                console.log(response);
             response.json().then(function(data) {
-                var cityName = data.name
-                var locationId = data.id
-                localStorage.setItem(cityName, locationId)
-        
-                $('.cityinfo .cityname').text(data.name);
-                $('.cityinfo .temp').text('Temp: ' + data.main.temp);
-                $('.cityinfo .wind').text('Wind: ' + data.wind.speed + ' MPH');
-                $('.cityinfo .humidity').text('Humidity: ' + data.main.humidity + '%');
-                // $('.cityinfo .uvindex').text('UV Index: ' + );
-                console.log(data);
+            $('.cityinfo #uv-index').text(data.daily[0].uvi);
+            console.log(data);
+            // add background colors to UV index text 
+            if (data.daily[0].uvi >= 0 && data.daily[0].uvi < 3) {
+                $('.cityinfo #uv-index').addClass("low");
+            } else if (data.daily[0].uvi >= 3 && data.daily[0].uvi < 6) {
+                $('.cityinfo #uv-index').addClass("moderate");
+            } else if (data.daily[0].uvi >= 6 && data.daily[0].uvi < 8) {
+                $('.cityinfo #uv-index').addClass("high");
+            } else if (data.daily[0].uvi >= 8 && data.daily[0].uvi < 10) {
+                $('.cityinfo #uv-index').addClass("veryhigh");
+            } else {
+                $('.cityinfo #uv-index').addClass("extreme");
+            }
+
+            // dynamically update 5-day forecast container
+            for (var i = 0; i < data.daily.length; i++) {
+                $('.wicon').eq(i).attr('src', 'http://openweathermap.org/img/wn/' + data.daily[i].weather[0].icon + '.png');
+                $('.forecast .temp').eq(i).text('Temp: ' + data.daily[i].temp.day);
+                $('.forecast .wind').eq(i).text('Wind: ' + data.daily[i].wind_speed + ' MPH');
+                $('.forecast .humidity').eq(i).text('Humidity: ' + data.daily[0].humidity + '%');
+            }
+
             })
         // request unsucessful 
         } else {
@@ -26,37 +38,43 @@ var getCityWeather = function(city) {
     });
 };
 
+// use old OneCall API to get latitude/longitude of user inputted city
+var getCityLatLon = function(city) {
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=6f634b702b3d948c5f118672eb3c519f&units=imperial';
+    fetch(apiUrl).then(function(response){
+        if (response.ok) {
+            response.json().then(function(data) {
+                var lat = data.coord.lat;
+                var lon = data.coord.lon;
+                var cityName = data.name
+                var locationId = data.id
+                localStorage.setItem(cityName, locationId)
+                console.log(data);
+
+                $('.cityinfo .cityname').text(data.name);
+                $('#wicon').attr('src', 'http://openweathermap.org/img/wn/' + data.weather[0].icon + '.png');
+                $('.cityinfo .temp').text('Temp: ' + data.main.temp + ' °F');
+                $('.cityinfo .wind').text('Wind: ' + data.wind.speed + ' MPH');
+                $('.cityinfo .humidity').text('Humidity: ' + data.main.humidity + '%');
+                getCityWeather(lat, lon);
+            })
+        }
+    })
+}
+
+
+
 
 $( '.submit' ).on("click", function() {
     var location = $( '.searchbox' ).val();
     if (location) {
-        getCityWeather(location)
+        getCityLatLon(location)
         } else {
         alert("Please enter a city name!");
     };
 });
 
 
-// var displayWeather = function (data) {
-//     ('.cityinfo .temp').text('Temp: ' + data.main.temp);
-//     ( '.cityinfo .wind' );
-//     ( '.cityinfo .humidity' );
-//     ( '.cityinfo .uvindex ');
-// }
-
-
-
-
-
-
-
-
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-
-
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
 
 
 // WHEN I view future weather conditions for that city
